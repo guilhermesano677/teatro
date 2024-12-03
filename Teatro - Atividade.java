2b68c70cd0d93b9c55f84b9c4e5961f184595cc4
guilhermesano677 <guilherme90221599@gmail.com>
2024-12-03 00:51:44 -0300

    Add files via upload
    C:\Users\sanor\Desktop\Teatro\PROJETO-JAVA\src

---
+++ Teatro - Atividade.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("\n********** BEM-VINDO AO SISTEMA **********\n");
+        
+        
+        System.out.println("Iniciando o sistema...");
+        Erros.iniciar_sistema("Sistema iniciado com sucesso");
+        
+        
+        Menu.exibirMenu();
+    }
+}
